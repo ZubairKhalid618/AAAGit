b25a4ff38a2332c494f2ee154e592cbb9aa72e3e
ZubairKhalid618 <zubairkhalid618@gmail.com>
2020-12-01 18:38:14 +0500

    Assignment 2,3

---
+++ Assignments/3/a.java
@@ -0,0 +1,14 @@
+class a{
+public static void main(String[] args) {
+	
+	//decleration
+	int a;
+	//assignment
+	a=10;
+	//expression (initialization of b)
+	int b=a+5;
+	System.out.println(b);
+
+}
+
+}
---
+++ Assignments/3/b.java
@@ -0,0 +1,14 @@
+class b{
+	public static void main(String[] args) {
+		
+		int a=1;
+
+		if (a==1) {
+			
+			System.out.println("If Executed");
+		}
+
+		else
+			System.out.println("Else Executed");
+	}
+}
